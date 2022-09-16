c9a6eb6ee752c6134ee98679ee99022357e33fd6
sss <kirillku4mar24@gmail.com>
2022-09-16 20:14:31 +0300

    Ipad presentation Fix

--- SwiftBookLessons/LessonList.swift
+++ SwiftBookLessons/LessonList.swift
@@ -16,41 +16,13 @@ class LessonList: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        labelConfigure()
-        labelMakeConstraints()
-        tableViewConfigure()
-        tableViewMakeConstraints()
-                
-    }
-    func labelConfigure() {
-        label.text = "SwiftBook Lessons"
-        label.font = UIFont.systemFont(ofSize: 25, weight: .bold)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(label)
-    }
-    func labelMakeConstraints() {
-        NSLayoutConstraint.activate([
-            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
-            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            label.widthAnchor.constraint(equalToConstant: 40)
-        ])
-    }
-    
-    func tableViewConfigure() {
+        title = "SwiftBook Lessons"
+        
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        tableView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(tableView)
-    }
-    func tableViewMakeConstraints() {
-        NSLayoutConstraint.activate([
-            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            tableView.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 16)
-        ])
+        tableView.frame = view.bounds
     }
 }
 
@@ -68,6 +40,6 @@ extension LessonList: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let detailItem = storyboard.instantiateViewController(identifier: lessons[indexPath.row])
-        present(detailItem, animated: true)
+        navigationController?.show(detailItem, sender: self)
     }
 }
